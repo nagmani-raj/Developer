1fef52bad48e0f29756b36db2eb314733054217b
nagmani-raj <nagmaniraj133@gmail.com>
2026-03-03 10:37:49 +0530

    second fresh code

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://developer-phcu.onrender.com";
 
 function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
